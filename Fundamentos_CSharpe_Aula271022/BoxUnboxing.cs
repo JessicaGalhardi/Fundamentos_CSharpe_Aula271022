44ce37d218c8a55837caab48cdb781b6b84d7774
JessicaGalhardi <104734490+JessicaGalhardi@users.noreply.github.com>
2022-10-31 15:53:47 +0100

    Adicionar arquivos de projeto.

---
+++ Fundamentos_CSharpe_Aula271022/BoxUnboxing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxingUnboxing;
+
+class BoxUnboxing
+{
+    static void Main(string[] args)
+    {
+        int x = 100;//variável do tipo primitivo(int)
+        object obj = x;//boxing i->0(int para object)
+        int j = (int)obj;//j recebe o unboxing de o
+
+        Console.WriteLine(" Unboxing realizado com sucesso!O valor de j é: " + j + " \n\r ");
+
+
+
+    }
+}
+
---
+++ TratamentoDeErros/BoxUnboxingErrors.cs
@@ -0,0 +1,29 @@
+﻿//__________________________________________________________________________
+//Tratando erros com boxing e unboxing
+
+try
+{
+    //Variável a é do tipo int, b do tipo float e recebe float. Essa divergência gera um erro.
+    int a = 001;
+    object objA = a;
+    float b = (float)objA;
+
+    throw new Exception("Lançado uma exception!");//Erro critico!
+
+}
+catch (InvalidCastException ex)//Tratando especificamente o erro de conversão!
+{
+
+    Console.WriteLine("Aconteceu um erro na aplicação da conversão de valores." + "\n\r"+ ex);
+}
+catch (Exception)//O catch tem como parâmentro a classe Exception
+{
+    Console.WriteLine(" Erro ao executar a aplicação não identificado! ");//Mensagem é exibida ao User em caso de erro.
+}
+
+//____________________________________________________________________________
+
+Console.ReadKey();
+    
+
+
